3fb3d11ab1fdc31c33e46339df25e531aedd8def
Santiago Martínez <xsantysxx@gmail.com>
2021-11-03 15:10:17 -0500

    Agregado busqueda

--- src/componentes/Objetos/ListaObjetos.jsx
+++ src/componentes/Objetos/ListaObjetos.jsx
@@ -1,21 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import CardObjeto from './CardObjeto'
 import { collection, getDocs } from 'firebase/firestore'
 import db from '../../base'
 
 export default function ListaObjetos() {
   const [objetos, setobjetos] = useState([])
+  const [buscar, setbuscar] = useState('')
 
   useEffect(() => {
-    obtenerObjetos(db)
-      .then((data) => {
-        return data
-      })
-      .then((data) => {
-        setobjetos(data)
-      })
+    if (buscar === '') {
+      obtenerObjetos(db)
+        .then((data) => {
+          return data
+        })
+        .then((data) => {
+          setobjetos(data)
+        })
+    }
 
-  }, [])
+  }, [buscar])
 
   const obtenerObjetos = async (db) => {
     const objetos = collection(db, 'objetos')
@@ -24,11 +27,52 @@ export default function ListaObjetos() {
     return listaObjetos
   }
 
+  const buscarObjeto = () => {
+    const referencia = buscar.toLowerCase()
+    setobjetos(
+      objetos.filter((objeto) =>
+        objeto.nombre.toLowerCase().includes(referencia),
+      ),
+    )
+  }
+
+  const handleChange = (e) => {
+    const { value } = e.target
+    setbuscar(value)
+  }
+
   return (
-    <div class="flex">
-      {objetos.map((objeto) => (
-        <CardObjeto data={objeto} />
-      ))}
+    <div class="flex flex-col">
+      <div className="flex items-center justify-center ">
+        <input
+          onChange={(e) => handleChange(e)}
+          className="border 1/5 h-5 px-3 py-5 mt-2 hover:outline-none rounded-md focus:ring-indigo-400"
+        />
+        <button
+          className="text-center ml-4 items-center justify-center"
+          onClick={buscarObjeto}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="flex flex-row">
+        {objetos.map((objeto) => (
+          <CardObjeto data={objeto} />
+        ))}
+      </div>
     </div>
   )
 }
