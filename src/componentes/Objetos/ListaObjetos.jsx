f10d960ee3b249539ef016194edea648c846fe81
Santiago Martínez <xsantysxx@gmail.com>
2021-11-03 12:47:37 -0500

    Revert "Mejoras"
    This reverts commit 31a70507021f34d3dfbf88c9072b74012e92d7c3.

--- src/componentes/Objetos/ListaObjetos.jsx
+++ src/componentes/Objetos/ListaObjetos.jsx
@@ -1,88 +1,34 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CardObjeto from './CardObjeto'
 import { collection, getDocs } from 'firebase/firestore'
 import db from '../../base'
 
 export default function ListaObjetos() {
   const [objetos, setobjetos] = useState([])
-  const [buscar, setbuscar] = useState('')
 
   useEffect(() => {
-    getDocs(collection(db, 'objetos'))
-      .then((data) => {
-        return data
-      })
-      .then((data) => {
-        const lista = data.docs.map((doc) => doc.data())
-        setobjetos(lista)
-      })
-  }, [])
+    const data = obtenerObjetos(db).then((data) => {
+      return data
+    })
 
-  useEffect(() => {
-    if(buscar === ''){
-      limpiar()
-    }
-  }, [buscar])
+    data.then((data) => {
+      setobjetos(data)
+    })
 
-  const buscarObjeto = () => {
-    setobjetos(
-      objetos.filter((objeto) =>
-        objeto.nombre
-          .toLowerCase()
-          .includes(buscar.toLowerCase()),
-      ),
-    )
-  }
+  })
 
-  const limpiar = () => {
-
-    setbuscar('');
-    getDocs(collection(db, 'objetos'))
-      .then((data) => {
-        return data
-      })
-      .then((data) => {
-        const lista = data.docs.map((doc) => doc.data())
-        setobjetos(lista)
-      })
-  }
-
-  const handleChange = (e)=>{
-    const {value} = e.target
-    setbuscar(value);
+  const obtenerObjetos = async (db) => {
+    const objetos = collection(db, 'objetos')
+    const objetosSnapshot = await getDocs(objetos)
+    const listaObjetos = objetosSnapshot.docs.map((doc) => doc.data())
+    return listaObjetos
   }
 
   return (
-    <div class="flex flex-col">
-      <div className="flex flex-row text-center items-center justify-center">
-        <input
-          type="text"
-          className="border w-1/4 h-5 px-3 py-5 mt-2 hover:outline-none rounded-md focus:ring-indigo-400"
-          placeholder="Buscar objeto..."
-          onChange={(e)=>handleChange(e)}
-        />
-        <button className="" onClick={buscarObjeto}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 my-auto mx-4"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-      </div>
-      <div className="flex flex-row justify-arround">
-        {objetos.map((objeto) => (
-          <CardObjeto data={objeto} />
-        ))}
-      </div>
+    <div class="flex">
+      {objetos.map((objeto) => (
+        <CardObjeto data={objeto} />
+      ))}
     </div>
   );
 }
--- src/componentes/Usuario/Perfil.jsx
+++ src/componentes/Usuario/Perfil.jsx
@@ -1,41 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import { useContext } from 'react/cjs/react.development'
-import db from '../../base'
-import { AuthContext } from '../Middlewares/AuthMiddleware'
-import { collection, getDocs, query, where } from '@firebase/firestore'
-import CardObjeto from '../Objetos/CardObjeto'
+import React from 'react'
 
 export default function Perfil() {
-  const { currentUser } = useContext(AuthContext)
-
-  const [email, setEmail] = useState(currentUser.email)
-  const [objetos, setobjetos] = useState([])
-
-  const objetosUsuario = () => {
-    const objetosRef = collection(db, 'objetos')
-    const q = query(objetosRef, where('usuario', '==', email))
-
-    getDocs(q)
-      .then((querySnapshot) => {
-        return querySnapshot
-      })
-      .then((data) => {
-        setobjetos(data.docs.map((doc) => doc.data()))
-      })
-  }
-
-  useEffect(() => {
-    objetosUsuario()
-  }, [])
-
   return (
-    <div class="flex flex-row justify-around">
+    <div class="p-10">
       <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <img
-          class="w-full"
-          src="https://bysperfeccionoral.com/wp-content/uploads/2020/01/136-1366211_group-of-10-guys-login-user-icon-png.jpg"
-          alt="foto"
-        />
+        <img class="w-full" src="https://bysperfeccionoral.com/wp-content/uploads/2020/01/136-1366211_group-of-10-guys-login-user-icon-png.jpg" alt="foto" />
         <div class="px-6 py-4">
           <label htmlFor="" className="block font-semibold">
             Nombre
@@ -67,11 +36,6 @@ export default function Perfil() {
             </button>
           </div>
         </div>
-      </div>
-      <div className="flex flex-row">
-        {objetos.map((objeto) => (
-          <CardObjeto data={objeto} />
-        ))}
       </div>
     </div>
   )
